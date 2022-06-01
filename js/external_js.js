52902a448ddae772498d2c4cd1804f8d7292fd81
Brian Carnes <carnes.brian.a@gmail.com>
2022-06-01 08:16:00 -0500

    inline/external js exercise

---
+++ js/external_js.js
@@ -0,0 +1,6 @@
+"use strict";
+
+console.log("Hello from external JavaScript!");
+alert("Welcome to my website");
+let userFavoriteColor = prompt("What is your favorite color?")
+alert(`Great, ${userFavoriteColor} is my favorite color too!`)
