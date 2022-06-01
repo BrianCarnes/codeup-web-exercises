3036b2bda35c28112b3c847115ad4ef7b5663c37
Brian Carnes <carnes.brian.a@gmail.com>
2022-06-01 10:16:47 -0500

    added functions for exercise three

--- js/output tests.js
+++ js/output tests.js
@@ -94,3 +94,16 @@ sample.replace("students", "Class");
 console.log(sample.indexOf("c")); //-1
 console.log(sample.indexOf("C")); //6
 console.log(sample.substring(sample.indexOf("C"),sample.indexOf("C")+6));
+
+
+let movies = (BB, LM, Herc) => (BB + LM + Herc)*3;
+console.log(movies(3,5,1));
+
+let pay = (Googz, Amaz, Facez) => (Googz*400)+(Amaz*380)+(Facez*350);
+console.log(pay(6,4,10));
+
+let enrollment = (full, conflict) => full === true && conflict === true ? console.log("Registered") : console.log("Error: conflict");
+enrollment(true, true);
+
+let productOffer = (items, expired, premium) => items >= 2 && expired === false || premium === true ? console.log("discount applied") : console.log("no discount");
+productOffer(1,true,true);
