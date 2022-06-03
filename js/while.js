fc7f340604621610172af47de16880fb57b3ef52
Brian Carnes <carnes.brian.a@gmail.com>
2022-06-03 11:54:42 -0500

    complete: while loop exercise functions.

---
+++ js/while.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// function twoTables() {
+//     let i = 2;
+//     while (i <= 65536) {
+//         console.log(i)
+//         i *= 2;
+//     }
+// }
+//
+// twoTables();
+
+// An ice cream seller can't go home until she sells all of her cones.
+// 1. First write enough code that generates a random number between 50 and 100 representing the amount of cones to sell before you start your loop.
+// 2. Inside the loop your code should generate another random number between 1 and 5, simulating the amount of cones being bought by her clients.
+// 3. Use a do-while loop to log to the console the amount of cones sold to each person. The below code shows how to get the random numbers for this exercise.
+
+function iceCreamStory() {
+    let allCones = Math.floor(Math.random() * 50) + 50;
+    let conesRemaining = 0;
+    let conesSold = 0
+    while (conesSold <= allCones) {
+        let oneThroughFive = Math.floor(Math.random() * 5) + 1;
+        conesRemaining = allCones - conesSold;
+        console.log(`${conesSold} cones sold...${conesRemaining} cones remain`);
+        if (oneThroughFive > conesRemaining && conesRemaining !== 0) {
+            console.log(`Sorry, I can only sell you ${conesRemaining} cones, it's all I have left`);
+            continue;
+        } else if (conesRemaining === 0) {
+            console.log("Yay! I sold them all!")
+        }
+        conesSold += oneThroughFive;
+    }
+}
+
+iceCreamStory();
