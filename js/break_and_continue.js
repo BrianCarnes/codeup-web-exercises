bda4d75c4748fb8db60505636beb07745e5d2e2d
Brian Carnes <carnes.brian.a@gmail.com>
2022-06-03 09:26:02 -0500

    fixed if error in break_and_continue.js

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -4,14 +4,14 @@
     function oddOneThroughFifty() {
         let userOddNumber = parseInt(prompt("Pick a number an odd number between 1-50"));
         for (let i = 1; i < 50; i++) {
-            if (i % 2 !== 0 && i !== userOddNumber) {
+            if (i % 2 !== 0) {
                 if (i === userOddNumber) {
                     console.log("Yikes! Skipping " + userOddNumber)
+                    continue;
                 }
                 console.log("Here is an odd number " + i);
                 continue;
             }
-            console.log('Here is a lovely even number: ' + i);
         }
     }
 
