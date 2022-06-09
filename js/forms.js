ec49bdc5724258d0d5ed331e38f18d8af2fec7d1
Brian Carnes <carnes.brian.a@gmail.com>
2022-06-09 11:30:16 -0500

    finished styling the forms website, huzzah

--- js/forms.js
+++ js/forms.js
@@ -6,8 +6,25 @@ function turnButtonGreen(event) {
 	document.getElementById("button").style.position = "relative";
 	document.getElementById("button").style.left = "50%";
 	document.getElementById("button").style.transform = "translateX(-50%)";
-	document.getElementById("button").style.backgroundColor = "#44c08a";
+	document.getElementById("button").style.backgroundColor = "#ecf0f3";
 	document.getElementById("button").style.borderRadius = "50%";
 	document.getElementById("btntext").style.opacity = "0";
 	document.getElementById("fa").style.opacity = "1";
 }
+
+let radios = document.querySelectorAll('input[type=radio][value="d"]');
+let other = document.getElementById("d");
+let options = document.getElementById("options");
+let radiogroup = document.getElementById("radiogroup");
+
+other.addEventListener("click", function () {
+	options.classList.toggle("show");
+	radiogroup.classList.toggle("show");
+});
+
+
+// radios.forEach(function(radio) {
+// 	radio.addEventListener("change", function () {
+// 		options.classList.toggle("show");
+// 	});
+// });
